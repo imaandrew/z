fd7888697c7c66bab1ea7c228c6e752e2b08551d
imaandrew <14946103+imaandrew@users.noreply.github.com>
2026-01-31 12:34:18 -0500

    add panic function

---
+++ src/core/panic.h
@@ -0,0 +1,51 @@
+#pragma once
+
+#include <format>
+#include <iostream>
+#include <source_location>
+#include <type_traits>
+
+#ifdef ENABLE_STACKTRACE
+#include <stacktrace>
+#endif
+
+template <typename T>
+concept NotStringView = !std::is_convertible_v<const T&, std::string_view>;
+
+template <typename... Args> struct PanicFormat {
+    std::format_string<Args...> fmt;
+    std::source_location loc;
+
+    template <typename Str>
+    explicit(NotStringView<Str>) consteval PanicFormat(
+        const Str& s,
+        const std::source_location& loc = std::source_location::current())
+        : fmt(s), loc(loc) {}
+};
+
+template <typename... Args>
+[[noreturn]] static void constexpr panic(
+    PanicFormat<std::type_identity_t<Args>...> fmt, Args&&... args) noexcept {
+    try {
+        std::cerr << std::format(
+            "{}:{} panic: {}\n", fmt.loc.file_name(), fmt.loc.line(),
+            std::format(fmt.fmt, std::forward<Args>(args)...));
+    } catch (...) {
+        std::cerr << fmt.loc.file_name() << ":" << fmt.loc.line()
+                  << " panic: formatting failed\n";
+    }
+
+#ifdef ENABLE_STACKTRACE
+    std::cerr << std::stacktrace::current() << '\n';
+#endif
+    std::cerr.flush();
+    std::abort();
+}
+
+template <typename... Args>
+static void constexpr expect(bool cond,
+                             PanicFormat<std::type_identity_t<Args>...> fmt,
+                             Args&&... args) noexcept {
+    if (!cond) [[unlikely]]
+        panic(fmt, std::forward<Args>(args)...);
+}
