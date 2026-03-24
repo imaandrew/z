47ee53241b2e6f082511065f01e8650b7a392762
imaandrew <14946103+imaandrew@users.noreply.github.com>
2026-03-24 17:03:35 -0400

    add more lexer tests

--- tests/test_lexer.cpp
+++ tests/test_lexer.cpp
@@ -1,7 +1,6 @@
 #include "diag/src_mgr.h"
 #include "lexer/lexer.h"
 #include "lexer/token.h"
-#include <algorithm>
 #include <gtest/gtest.h>
 #include <memory>
 #include <string>
@@ -49,7 +48,7 @@ TEST_F(LexerTest, LexesKeywords) {
     EXPECT_EQ(tokens[3].get_kind(), TokenKind::KwConst);
     EXPECT_EQ(tokens[4].get_kind(), TokenKind::KwReturn);
     EXPECT_EQ(tokens[5].get_kind(), TokenKind::KwWhile);
-    EXPECT_EQ(tokens.back().get_kind(), TokenKind::Eof);
+    EXPECT_EQ(tokens[6].get_kind(), TokenKind::Eof);
 }
 
 TEST_F(LexerTest, LexesSingleCharOperators) {
@@ -64,7 +63,7 @@ TEST_F(LexerTest, LexesSingleCharOperators) {
     EXPECT_EQ(tokens[5].get_kind(), TokenKind::LBracket);
     EXPECT_EQ(tokens[6].get_kind(), TokenKind::Slash);
     EXPECT_EQ(tokens[7].get_kind(), TokenKind::Star);
-    EXPECT_EQ(tokens.back().get_kind(), TokenKind::Eof);
+    EXPECT_EQ(tokens[8].get_kind(), TokenKind::Eof);
 }
 
 TEST_F(LexerTest, LexesMultiCharOperators) {
@@ -78,7 +77,7 @@ TEST_F(LexerTest, LexesMultiCharOperators) {
     EXPECT_EQ(tokens[4].get_kind(), TokenKind::RangeEq);
     EXPECT_EQ(tokens[5].get_kind(), TokenKind::Or);
     EXPECT_EQ(tokens[6].get_kind(), TokenKind::ShlEq);
-    EXPECT_EQ(tokens.back().get_kind(), TokenKind::Eof);
+    EXPECT_EQ(tokens[7].get_kind(), TokenKind::Eof);
 }
 
 TEST_F(LexerTest, LexesMixedOperators) {
@@ -93,7 +92,7 @@ TEST_F(LexerTest, LexesMixedOperators) {
     EXPECT_EQ(tokens[5].get_kind(), TokenKind::AndAnd);
     EXPECT_EQ(tokens[6].get_kind(), TokenKind::ShrEq);
     EXPECT_EQ(tokens[7].get_kind(), TokenKind::Caret);
-    EXPECT_EQ(tokens.back().get_kind(), TokenKind::Eof);
+    EXPECT_EQ(tokens[8].get_kind(), TokenKind::Eof);
 }
 
 TEST_F(LexerTest, DoesNotLexMultiCharOpAcrossWhitespace) {
@@ -107,7 +106,7 @@ TEST_F(LexerTest, DoesNotLexMultiCharOpAcrossWhitespace) {
     EXPECT_EQ(tokens[4].get_kind(), TokenKind::Eq);
     EXPECT_EQ(tokens[5].get_kind(), TokenKind::Gt);
     EXPECT_EQ(tokens[6].get_kind(), TokenKind::Gt);
-    EXPECT_EQ(tokens.back().get_kind(), TokenKind::Eof);
+    EXPECT_EQ(tokens[7].get_kind(), TokenKind::Eof);
 }
 
 TEST_F(LexerTest, LexesIdentifiers) {
@@ -119,10 +118,116 @@ TEST_F(LexerTest, LexesIdentifiers) {
     EXPECT_EQ(tokens[1].get_kind(), TokenKind::Identifier);
     EXPECT_EQ(tokens[2].get_kind(), TokenKind::Identifier);
     EXPECT_EQ(tokens[3].get_kind(), TokenKind::Identifier);
-    EXPECT_EQ(tokens.back().get_kind(), TokenKind::Eof);
+    EXPECT_EQ(tokens[4].get_kind(), TokenKind::Eof);
 
     EXPECT_EQ(l.source->get_string(tokens[0].get_span()), "test");
     EXPECT_EQ(l.source->get_string(tokens[1].get_span()), "ident");
     EXPECT_EQ(l.source->get_string(tokens[2].get_span()), "z003a");
     EXPECT_EQ(l.source->get_string(tokens[3].get_span()), "_b8__938");
+}
+
+TEST_F(LexerTest, LexesNumbers) {
+    auto l = LexerWithSource("31 0x04423 0b11001 0.344 0O314");
+    auto tokens = l.lex();
+
+    ASSERT_EQ(tokens.size(), 6);
+    EXPECT_EQ(tokens[0].get_kind(), TokenKind::Number);
+    EXPECT_EQ(tokens[1].get_kind(), TokenKind::Number);
+    EXPECT_EQ(tokens[2].get_kind(), TokenKind::Number);
+    EXPECT_EQ(tokens[3].get_kind(), TokenKind::Number);
+    EXPECT_EQ(tokens[4].get_kind(), TokenKind::Number);
+    EXPECT_EQ(tokens[5].get_kind(), TokenKind::Eof);
+
+    EXPECT_EQ(l.source->get_string(tokens[0].get_span()), "31");
+    EXPECT_EQ(l.source->get_string(tokens[1].get_span()), "0x04423");
+    EXPECT_EQ(l.source->get_string(tokens[2].get_span()), "0b11001");
+    EXPECT_EQ(l.source->get_string(tokens[3].get_span()), "0.344");
+    EXPECT_EQ(l.source->get_string(tokens[4].get_span()), "0O314");
+}
+
+TEST_F(LexerTest, IgnoresUnderscoresInNumbers) {
+    auto l = LexerWithSource("123_456_789 0x8040_0138 0b1101_1100");
+    auto tokens = l.lex();
+
+    ASSERT_EQ(tokens.size(), 4);
+    EXPECT_EQ(tokens[0].get_kind(), TokenKind::Number);
+    EXPECT_EQ(tokens[1].get_kind(), TokenKind::Number);
+    EXPECT_EQ(tokens[2].get_kind(), TokenKind::Number);
+    EXPECT_EQ(tokens[3].get_kind(), TokenKind::Eof);
+
+    EXPECT_EQ(l.source->get_string(tokens[0].get_span()), "123_456_789");
+    EXPECT_EQ(l.source->get_string(tokens[1].get_span()), "0x8040_0138");
+    EXPECT_EQ(l.source->get_string(tokens[2].get_span()), "0b1101_1100");
+}
+
+TEST_F(LexerTest, LexesNestedStrings) {
+    auto l = LexerWithSource(R"("\"test string\"")");
+    auto tokens = l.lex();
+
+    ASSERT_EQ(tokens.size(), 2);
+    EXPECT_EQ(tokens[0].get_kind(), TokenKind::String);
+    EXPECT_EQ(tokens[1].get_kind(), TokenKind::Eof);
+
+    EXPECT_EQ(l.source->get_string(tokens[0].get_span()), R"(\"test string\")");
+}
+
+TEST_F(LexerTest, LexesChars) {
+    auto l = LexerWithSource("'a' 'b' 'c'");
+    auto tokens = l.lex();
+
+    ASSERT_EQ(tokens.size(), 4);
+    EXPECT_EQ(tokens[0].get_kind(), TokenKind::Char);
+    EXPECT_EQ(tokens[1].get_kind(), TokenKind::Char);
+    EXPECT_EQ(tokens[2].get_kind(), TokenKind::Char);
+    EXPECT_EQ(tokens[3].get_kind(), TokenKind::Eof);
+
+    EXPECT_EQ(l.source->get_string(tokens[0].get_span()), "a");
+    EXPECT_EQ(l.source->get_string(tokens[1].get_span()), "b");
+    EXPECT_EQ(l.source->get_string(tokens[2].get_span()), "c");
+}
+
+TEST_F(LexerTest, IgnoresSingleLineComments) {
+    auto tokens = lex_string("test 1 // this should be ignored\nnot this");
+
+    ASSERT_EQ(tokens.size(), 5);
+    EXPECT_EQ(tokens[0].get_kind(), TokenKind::Identifier);
+    EXPECT_EQ(tokens[1].get_kind(), TokenKind::Number);
+    EXPECT_EQ(tokens[2].get_kind(), TokenKind::Identifier);
+    EXPECT_EQ(tokens[3].get_kind(), TokenKind::Identifier);
+    EXPECT_EQ(tokens[4].get_kind(), TokenKind::Eof);
+}
+
+TEST_F(LexerTest, IgnoresMultiLineComments) {
+    auto tokens =
+        lex_string("test 1 /* this should \nall be\nignored */ not this");
+
+    ASSERT_EQ(tokens.size(), 5);
+    EXPECT_EQ(tokens[0].get_kind(), TokenKind::Identifier);
+    EXPECT_EQ(tokens[1].get_kind(), TokenKind::Number);
+    EXPECT_EQ(tokens[2].get_kind(), TokenKind::Identifier);
+    EXPECT_EQ(tokens[3].get_kind(), TokenKind::Identifier);
+    EXPECT_EQ(tokens[4].get_kind(), TokenKind::Eof);
+}
+
+TEST_F(LexerTest, HandlesNestedComments) {
+    auto tokens = lex_string(
+        "test 1 /* this should be ignored /* this too */ */ not this");
+
+    ASSERT_EQ(tokens.size(), 5);
+    EXPECT_EQ(tokens[0].get_kind(), TokenKind::Identifier);
+    EXPECT_EQ(tokens[1].get_kind(), TokenKind::Number);
+    EXPECT_EQ(tokens[2].get_kind(), TokenKind::Identifier);
+    EXPECT_EQ(tokens[3].get_kind(), TokenKind::Identifier);
+    EXPECT_EQ(tokens[4].get_kind(), TokenKind::Eof);
+}
+
+TEST_F(LexerTest, LexesStrings) {
+    auto l = LexerWithSource("\"test string\"");
+    auto tokens = l.lex();
+
+    ASSERT_EQ(tokens.size(), 2);
+    EXPECT_EQ(tokens[0].get_kind(), TokenKind::String);
+    EXPECT_EQ(tokens[1].get_kind(), TokenKind::Eof);
+
+    EXPECT_EQ(l.source->get_string(tokens[0].get_span()), "test string");
 }
